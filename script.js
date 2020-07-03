1b6e1e402abb9bc321a7a212c0eebb3741fac6a4
p4nash <nash.riaz1995@gmail.com>
2020-07-03 03:05:27 +0100

    Usernames highlighted in events

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var usersInChat = [];
-var channelNameString = "p4nash";
+var channelNameString = "thefreakiestfox";
 
 $(function(){
 console.log("I'm ready, Mr. Krabs!");
@@ -15,7 +15,7 @@ ComfyJS.onJoin=(user, self, extra) => {
     console.log(user+' joined chat');
 
     AddEvent('https://img.icons8.com/material/24/42ff87/chat--v1.png',
-    user+' just joined.');
+    "<span class='userEvent'>"+user+'</span> just joined.');
 
     if(!usersInChat.find(findUsername, user)){
       usersInChat.push(user);
@@ -27,7 +27,7 @@ ComfyJS.onPart=(user, self, extra) => {
     console.log(user+' left chat');
 
     AddEvent('https://img.icons8.com/material/24/ff6ba1/chat--v1.png',
-    user+' just left.');
+    "<span class='userEvent'>"+user+'</span> just left.');
 
     if(usersInChat.find(findUsername, user))
     {
@@ -43,35 +43,35 @@ ComfyJS.onHosted=(user, viewers, autohost, extra) => {
     console.log(user+' hosted');
 
     AddEvent('https://img.icons8.com/android/24/ff6ba1/retro-tv.png',
-    user+' hosted with '+viewers+' viewers.');
+    "<span class='userEvent'>"+user+'</span> hosted with '+viewers+' viewers.');
 }
 
 ComfyJS.onRaid=(user, viewers, extra) => {
     console.log(user+' raided');
 
     AddEvent('https://img.icons8.com/ios-glyphs/30/ff6ba1/baby-footprints-path.png',
-    user+' raided with '+viewers+' viewers.');
+    "<span class='userEvent'>"+user+'</span> raided with '+viewers+' viewers.');
 }
 
 ComfyJS.onCheer=(user, message, bits, flags, extra) => {
     console.log(user+' cheered');
 
     AddEvent('https://img.icons8.com/ios-filled/50/42ff87/diamond--v1.png',
-    user+' cheered '+bits+' bits. They said: <span class="bold">"'+message+'"</span>.');
+    "<span class='userEvent'>"+user+'</span> cheered '+bits+' bits. They said: <span class="bold">"'+message+'"</span>.');
 }
 
 ComfyJS.onSub=(user, message, subTierInfo, extra) => {
     console.log(user+' subbed. ', subTierInfo.planName);
 
     AddEvent('https://img.icons8.com/material-sharp/24/42ff87/star.png',
-    user+' subbed with '+subTierInfo.planName+' sub. They said: <span class="italic">"'+message+'"</span>.');
+    "<span class='userEvent'>"+user+'</span> subbed with '+subTierInfo.planName+' sub. They said: <span class="italic">"'+message+'"</span>.');
 }
 
 ComfyJS.onResub=(user, message, streamMonths, cumulativeMonths, subTierInfo, extra) => {
     console.log(user+' subbed. ', subTierInfo.planName);
 
     AddEvent('https://img.icons8.com/material-sharp/24/ffd000/star.png',
-    user+' subbed with '+subTierInfo.planName+' sub. They\'ve been subbed for  <span class="bold">'+streamMonths+' months</span>.'+
+    "<span class='userEvent'>"+user+'</span> subbed with '+subTierInfo.planName+' sub. They\'ve been subbed for  <span class="bold">'+streamMonths+' months</span>.'+
      +'They said: <span class="italic">"'+message+'"</span>.');
 }
 
@@ -79,21 +79,21 @@ ComfyJS.onSubGift=( gifterUser, streakMonths, recipientUser, senderCount, subTie
     console.log(user+' subbed. ', subTierInfo.planName);
 
     AddEvent('https://img.icons8.com/metro/26/42ff87/gift.png',
-    gifterUser+' gifted a '+subTierInfo.planName+' sub to '+recipientUser+'. They\'ve gifted '+senderCount+' subs so far!');
+    "<span class='userEvent'>"+gifterUser+'</span> gifted a '+subTierInfo.planName+' sub to '+recipientUser+'. They\'ve gifted '+senderCount+' subs so far!');
 }
 
 ComfyJS.onGiftSubContinue=(user, sender, extra) => {
     console.log(user+' resubbed');
 
     AddEvent('https://img.icons8.com/metro/26/ffd000/gift.png',
-    sender+' continued '+user+'\'s sub for them!');
+    "<span class='userEvent'>"+sender+'</span> continued '+user+'\'s sub for them!');
 }
 
 ComfyJS.onSubMysteryGift=( gifterUser, numbOfSubs, senderCount, subTierInfo, extra ) => {
     console.log(user+' subbed. ', subTierInfo.planName);
 
     AddEvent('https://img.icons8.com/pastel-glyph/64/42ff87/christmas-gift--v2.png',
-    gifterUser+' gifted '+numbOfSubs+' '+subTierInfo.planName+' subs to the channel! They\'ve gifted '+senderCount+' to the channel.');
+    "<span class='userEvent'>"+gifterUser+'</span> gifted '+numbOfSubs+' '+subTierInfo.planName+' subs to the channel! They\'ve gifted '+senderCount+' to the channel.');
 }
 
 ComfyJS.Init( channelNameString );
